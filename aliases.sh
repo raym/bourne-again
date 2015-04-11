871c5508e2c189cb17d1f8fadc8f90e88c9cd674
raym <a4265622@opayq.com>
2015-04-10 19:46:17 -0700

    organize further, and keep a list of prompt escape characters

---
+++ aliases.sh
@@ -0,0 +1,11 @@
+alias l="ls -halp"
+alias ls="ls -Gp"
+alias ll="ls -hlp"
+alias la="ls -ap"
+
+alias h=history
+alias cd..="cd .."
+
+alias st="git st"
+alias v="vim ."
+alias devmeteor="~/src/meteor/meteor"
---
+++ vendor.sh
@@ -0,0 +1,14 @@
+if [ -f $HOME/.git-completion.bash ]; then
+  . $HOME/.git-completion.bash
+fi
+
+if [ -f $HOME/.bash/git-aware-prompt/prompt.sh ]; then
+  . $HOME/.bash/git-aware-prompt/prompt.sh
+else
+  echo -e "git-aware-prompt not installed. to install:"
+  echo -e "[31;40m"
+  echo "  mkdir ~/.bash"
+  echo "  cd ~/.bash"
+  echo "  git clone https://github.com/jimeh/git-aware-prompt.git"
+  echo -n "[0m"
+fi
