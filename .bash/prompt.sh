3109c2f1dc33c232757c1f6b0a500ccad66c1a78
raym <a4265622@opayq.com>
2015-04-10 19:02:53 -0700

    create prompt for sudo

--- .bash/prompt.sh
+++ .bash/prompt.sh
@@ -2,4 +2,5 @@ PS1="
 \[$IGreen\]\w
 \[$IRed\]jobs: \[$IYellow\]\j \[$ICyan\]\$git_branch\[$IRed\]\$git_dirty\[$IPurple\]\$\[$Color_Off\] "
 
-export SUDO_PS1="\[$bakred\]\u@\h\[$txtrst\] \w\$ "
+SUDO_PS1="\[$On_IRed\]\u\[$Color_Off\]@\[$On_IPurple\]\h\[$Color_Off\] \[$On_IGreen\]\w\[$On_IBlack\]\[$Color_Off\] \[$IGreen\]\$ \[$Color_Off\]"
+export SUDO_PS1
